a9feb5f86c9c924ab8814e282e3e583549db15de
Kevin Postreich <kevinlp@us.ibm.com>
2021-11-08 11:43:43 -0500

    ipdate deploy.sh in tekton lab

--- labs/Openshift/DevopsManagement/tekton-pipeline/deploy.sh
+++ labs/Openshift/DevopsManagement/tekton-pipeline/deploy.sh
@@ -3,4 +3,4 @@ oc create -f git-clone.yaml
 oc create -f buildah.yaml
 oc create -f oc-deploy.yaml
 oc create -f pipeline-deploy.yaml
-oc create -f pipeline-deploy-run.yaml
+#oc create -f pipeline-deploy-run.yaml
